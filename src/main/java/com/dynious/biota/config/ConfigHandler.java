b953aaa402f98deb4902bf4304b94435dd228841
Dynious <iDynious@gmail.com>
2015-02-21 21:24:12 +0100

    Config stuffs, finally done with this :P

--- src/main/java/com/dynious/biota/config/ConfigHandler.java
+++ src/main/java/com/dynious/biota/config/ConfigHandler.java
@@ -1,6 +1,11 @@
 package com.dynious.biota.config;
 
+import com.dynious.biota.config.annotations.ConfigBooleanValue;
+import com.dynious.biota.config.annotations.ConfigDoubleValue;
+import com.dynious.biota.config.annotations.ConfigFloatValue;
+import com.dynious.biota.config.annotations.ConfigIntValue;
 import com.dynious.biota.lib.Reference;
+import com.dynious.biota.lib.Settings;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -8,6 +13,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.config.Configuration;
 
 import java.io.File;
+import java.lang.reflect.Field;
 
 public class ConfigHandler
 {
@@ -22,19 +28,53 @@ public class ConfigHandler
 
     public static void syncConfig()
     {
+        for (Field field : Settings.class.getDeclaredFields())
+        {
+            try
+            {
+                if (field.isAnnotationPresent(ConfigBooleanValue.class))
+                {
+                    ConfigBooleanValue annotation = field.getAnnotation(ConfigBooleanValue.class);
+                    if (annotation.comment().isEmpty())
+                        field.setBoolean(null, configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), annotation.defaultValue()).getBoolean(annotation.defaultValue()));
+                    else
+                        field.setBoolean(null, configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), annotation.defaultValue(), annotation.comment()).getBoolean(annotation.defaultValue()));
+                }
+                else if (field.isAnnotationPresent(ConfigIntValue.class))
+                {
+                    ConfigIntValue annotation = field.getAnnotation(ConfigIntValue.class);
+                    if (annotation.comment().isEmpty())
+                        field.setInt(null, configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), annotation.defaultValue()).getInt(annotation.defaultValue()));
+                    else
+                        field.setInt(null, configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), annotation.defaultValue(), annotation.comment()).getInt(annotation.defaultValue()));
+                }
+                else if (field.isAnnotationPresent(ConfigFloatValue.class))
+                {
+                    ConfigFloatValue annotation = field.getAnnotation(ConfigFloatValue.class);
+                    //Hacky way to prevent float to double conversion from 'losing precision'
+                    double defaultValue = Double.parseDouble(Float.toString(annotation.defaultValue()));
+                    if (annotation.comment().isEmpty())
+                        field.setFloat(null, (float) configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), defaultValue).getDouble(defaultValue));
+                    else
+                        field.setFloat(null, (float) configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), defaultValue, annotation.comment()).getDouble(defaultValue));
+                }
+                else if (field.isAnnotationPresent(ConfigDoubleValue.class))
+                {
+                    ConfigDoubleValue annotation = field.getAnnotation(ConfigDoubleValue.class);
+                    if (annotation.comment().isEmpty())
+                        field.set(null, configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), annotation.defaultValue()).getDouble(annotation.defaultValue()));
+                    else
+                        field.set(null, configFile.get(Configuration.CATEGORY_GENERAL, field.getName().replace('_', ' '), annotation.defaultValue(), annotation.comment()).getDouble(annotation.defaultValue()));
+                }
+            }
+            catch (IllegalAccessException e)
+            {
+                e.printStackTrace();
+            }
+        }
 
         if (configFile.hasChanged())
             configFile.save();
-    }
-
-    private static int getConfiguration(String setting, int defaultSetting, String comment)
-    {
-        return configFile.get(Configuration.CATEGORY_GENERAL, setting, defaultSetting, comment).getInt(defaultSetting);
-    }
-
-    private static boolean getConfiguration(String setting, boolean defaultSetting, String comment)
-    {
-        return configFile.get(Configuration.CATEGORY_GENERAL, setting, defaultSetting, comment).getBoolean(defaultSetting);
     }
 
     @SubscribeEvent
--- src/main/java/com/dynious/biota/lib/Settings.java
+++ src/main/java/com/dynious/biota/lib/Settings.java
@@ -1,58 +1,99 @@
 package com.dynious.biota.lib;
 
+import com.dynious.biota.config.annotations.ConfigFloatValue;
+import com.dynious.biota.config.annotations.ConfigIntValue;
+
 public class Settings
 {
-    public static final float NORMAL_PHOSPHORUS = 15F;
-    public static final float DELTA_PHOSPHORUS = 5F;
+    @ConfigFloatValue(defaultValue = 15F, comment = "Normal Phosphorus amount (ppm)")
+    public static float NORMAL_PHOSPHORUS;
+    @ConfigFloatValue(defaultValue = 5F, comment = "The random difference in Phosphorus amount per chunk")
+    public static float DELTA_PHOSPHORUS;
 
-    public static final float NORMAL_POTASSIUM = 200F;
-    public static final float DELTA_POTASSIUM = 50F;
+    @ConfigFloatValue(defaultValue = 200F, comment = "Normal Potassium amount (ppm)")
+    public static float NORMAL_POTASSIUM;
+    @ConfigFloatValue(defaultValue = 50F, comment = "The random difference in Potassium amount per chunk")
+    public static float DELTA_POTASSIUM;
 
-    public static final float NORMAL_NITROGEN = 7.5F;
-    public static final float DELTA_NITROGEN = 2.5F;
+    @ConfigFloatValue(defaultValue = 7.5F, comment = "Normal Nitrogen amount (ppm)")
+    public static float NORMAL_NITROGEN;
+    @ConfigFloatValue(defaultValue = 2.5F, comment = "The random difference in Nitrogen amount per chunk")
+    public static float DELTA_NITROGEN;
 
-    public static final float MAX_PLANT_GROWTH = 1F;
-    public static final float NORMAL_PLANT_GROWTH = 0.75F;
-    public static final float MINIMAL_PLANT_GROWTH = 0F;
+    @ConfigFloatValue(defaultValue = 1F, comment = "Maximum plant growth speed (compared to non-modified) - max = 1")
+    public static float MAX_PLANT_GROWTH;
+    @ConfigFloatValue(defaultValue = 0.75F, comment = "Normal plant growth speed (compared to non-modified) - max = 1")
+    public static float NORMAL_PLANT_GROWTH;
+    @ConfigFloatValue(defaultValue = 0F, comment = "Minimal plant growth speed (compared to non-modified) - max = 1")
+    public static float MINIMAL_PLANT_GROWTH;
 
-    public static final float NUTRIENT_ABUNDANCE_FOR_SPREAD = 1.5F;
-    public static final float NUTRIENT_ABUNDANCE_FOR_MAX_GROWTH = 1.5F;
-    public static final float NUTRIENT_AMOUNT_FOR_NORMAL_GROWTH = 1.0F;
-    public static final float NUTRIENT_SHORTAGE_FOR_STOP_GROWTH = 0.75F;
-    public static final float NUTRIENT_SHORTAGE_FOR_DEATH = 0.35F;
+    @ConfigFloatValue(defaultValue = 1.5F, comment = "Nutrient amount amount needed for plant spread (compared to normal)")
+    public static float NUTRIENT_ABUNDANCE_FOR_SPREAD;
+    @ConfigFloatValue(defaultValue = 1.5F, comment = "Nutrient amount amount needed for plant max growth (compared to normal)")
+    public static float NUTRIENT_ABUNDANCE_FOR_MAX_GROWTH;
+    @ConfigFloatValue(defaultValue = 1.0F, comment = "Nutrient amount amount needed for plant normal growth (compared to normal)")
+    public static float NUTRIENT_AMOUNT_FOR_NORMAL_GROWTH;
+    @ConfigFloatValue(defaultValue = 0.75F, comment = "Nutrient amount lower than this will stop plant growth (compared to normal)")
+    public static float NUTRIENT_SHORTAGE_FOR_STOP_GROWTH;
+    @ConfigFloatValue(defaultValue = 0.35F, comment = "Nutrient amount lower than this will kill plants (compared to normal)")
+    public static float NUTRIENT_SHORTAGE_FOR_DEATH;
 
-    public static final float LIGHT_VALUE_FOR_SPREAD = 12;
-    public static final float LIGHT_VALUE_FOR_MAX_GROWTH = 12;
-    public static final float LIGHT_VALUE_FOR_NORMAL_GROWTH = 10;
-    public static final float LIGHT_VALUE_FOR_STOP_GROWTH = 8;
-    public static final float LIGHT_VALUE_FOR_DEATH = 6;
+    @ConfigIntValue(defaultValue = 12, comment = "Light amount amount needed for plant spread (0 - 15)")
+    public static int LIGHT_VALUE_FOR_SPREAD;
+    @ConfigIntValue(defaultValue = 12, comment = "Light amount amount needed for plant max growth (0 - 15)")
+    public static int LIGHT_VALUE_FOR_MAX_GROWTH;
+    @ConfigIntValue(defaultValue = 10, comment = "Light amount amount needed for plant normal growth (0 - 15)")
+    public static int LIGHT_VALUE_FOR_NORMAL_GROWTH;
+    @ConfigIntValue(defaultValue = 8, comment = "Light amount lower than this will stop plant growth (0 - 15)")
+    public static int LIGHT_VALUE_FOR_STOP_GROWTH;
+    @ConfigIntValue(defaultValue = 6, comment = "Light amount lower than this will kill plants (0 - 15)")
+    public static int LIGHT_VALUE_FOR_DEATH;
 
-    public static final float PLANT_SPREAD_CHANCE = 0.05F;
+    @ConfigFloatValue(defaultValue = 0.1F, comment = "The chance plants growth when ticking (1 tick per +- 68 secs) - max 1")
+    public static float PLANT_SPREAD_CHANCE;
 
-    public static final int TICKS_PER_BIOSYSTEM_UPDATE = 200;
+    @ConfigIntValue(defaultValue = 200, comment = "Ticks between updates in biosystems, higher is better for performance, but will make changes show up later")
+    public static int TICKS_PER_BIOSYSTEM_UPDATE;
+
+    @ConfigIntValue(defaultValue = 24000, comment = "The amount of ticks for full change is nutrient values")
+    public static int BIOSYSTEM_NUTRIENT_CHANGE_TICKS;
 
     //24000 ticks per MC day. One day for 1.0 change.
-    public static final float BIOSYSTEM_CHANGE_RATE = (float) TICKS_PER_BIOSYSTEM_UPDATE /24000;
+    public static float BIOSYSTEM_CHANGE_RATE = (float) TICKS_PER_BIOSYSTEM_UPDATE / BIOSYSTEM_NUTRIENT_CHANGE_TICKS;
 
     //Mass fraction of nutrients in biomass
-    public static final float BIOMASS_PHOSPHORUS_RATE = 0.001F;
-    public static final float BIOMASS_POTASSIUM_RATE = 0.006F;
-    public static final float BIOMASS_NITROGEN_RATE = 0.014F;
+    @ConfigFloatValue(defaultValue = 0.001F, comment = "The amount of Phosphorus per biomass value")
+    public static float BIOMASS_PHOSPHORUS_RATE;
+    @ConfigFloatValue(defaultValue = 0.006F, comment = "The amount of Potassium per biomass value")
+    public static float BIOMASS_POTASSIUM_RATE;
+    @ConfigFloatValue(defaultValue = 0.014F, comment = "The amount of Nitrogen per biomass value")
+    public static float BIOMASS_NITROGEN_RATE;
+    
+    private static float NORMAL_RATE = (float) Math.cbrt(1.0 / (BIOMASS_PHOSPHORUS_RATE * BIOMASS_POTASSIUM_RATE * BIOMASS_NITROGEN_RATE));
+    
+    public static float PHOSPHORUS_CHANGE_RATE = BIOMASS_PHOSPHORUS_RATE * NORMAL_RATE * BIOSYSTEM_CHANGE_RATE;
+    public static float POTASSIUM_CHANGE_RATE = BIOMASS_POTASSIUM_RATE * NORMAL_RATE * BIOSYSTEM_CHANGE_RATE;
+    public static float NITROGEN_CHANGE_RATE = BIOMASS_NITROGEN_RATE * NORMAL_RATE * BIOSYSTEM_CHANGE_RATE;
 
-    private static final float NORMAL_RATE = (float) Math.cbrt(1.0 / (BIOMASS_PHOSPHORUS_RATE * BIOMASS_POTASSIUM_RATE * BIOMASS_NITROGEN_RATE));
+    @ConfigFloatValue(defaultValue = 10F, comment = "The speed at which bacteria spread compared to the average change of nutrients")
+    public static float BACTERIA_CHANGE_AMOUNT;
+    
+    public static float BACTERIA_CHANGE_RATE = BACTERIA_CHANGE_AMOUNT*BIOSYSTEM_CHANGE_RATE;
 
-    public static final float PHOSPHORUS_CHANGE_RATE = BIOMASS_PHOSPHORUS_RATE * NORMAL_RATE * BIOSYSTEM_CHANGE_RATE;
-    public static final float POTASSIUM_CHANGE_RATE = BIOMASS_POTASSIUM_RATE * NORMAL_RATE * BIOSYSTEM_CHANGE_RATE;
-    public static final float NITROGEN_CHANGE_RATE = BIOMASS_NITROGEN_RATE * NORMAL_RATE * BIOSYSTEM_CHANGE_RATE;
-    public static final float BACTERIA_CHANGE_RATE = 10F*BIOSYSTEM_CHANGE_RATE;
+    @ConfigIntValue(defaultValue = 168000, comment = "The amount of ticks for full spread is nutrient values")
+    public static int BIOSYSTEM_NUTRIENT_SPREAD_TICKS;
 
     //168000 ticks per MC week. One week for 1.0 change in spread.
-    public static final float BIOSYSTEM_SPREAD_RATE = (float) TICKS_PER_BIOSYSTEM_UPDATE /168000;
+    public static float BIOSYSTEM_SPREAD_RATE = (float) TICKS_PER_BIOSYSTEM_UPDATE / BIOSYSTEM_NUTRIENT_SPREAD_TICKS;
 
     // Bacteria will start to die when there's this much biomass compared to what they can consume at max
-    public static final float BACTERIA_DEATH = 0.75F;
+    @ConfigFloatValue(defaultValue = 0.75F, comment = "Bacteria will start to die when there's this much biomass compared to what they can consume at max")
+    public static float BACTERIA_DEATH;
 
-    public static final float BONEMEAL_PHOSPHORUS = 1.2F;
-    public static final float BONEMEAL_POTASSIUM = 0.0F;
-    public static final float BONEMEAL_NITROGEN = 0.4F;
+    @ConfigFloatValue(defaultValue = 1.2F, comment = "The amount Phosphorus in Bonemeal")
+    public static float BONEMEAL_PHOSPHORUS;
+    @ConfigFloatValue(defaultValue = 0.0F, comment = "The amount Potassium in Bonemeal")
+    public static float BONEMEAL_POTASSIUM;
+    @ConfigFloatValue(defaultValue = 0.4F, comment = "The amount Nitrogen in Bonemeal")
+    public static float BONEMEAL_NITROGEN;
 }
